bd749745a91968c846a5040209a06a628f092029
vamshisai98 <saivamshianirudh136@gmail.com>
2024-03-30 12:07:00 +0530

    navigation and footer mobile responsive finished

--- src/common/Logo/Logo.jsx
+++ src/common/Logo/Logo.jsx
@@ -3,7 +3,7 @@ import logoImage from '../../assets/logo.jpg'
 
 const Logo = () => {
   return (
-    <div><img src={logoImage} alt="" /></div>
+    <div><img src={logoImage} alt="logo" /></div>
   )
 }
 
--- src/common/Navbar/Navbar.jsx
+++ src/common/Navbar/Navbar.jsx
@@ -1,30 +1,74 @@
-import React from 'react'
-
+import React, { useState } from 'react';
 import Logo from '../Logo/Logo';
-
-import { Grid, Box } from '@mui/material';
+import { Grid, Box, IconButton, Drawer, List, ListItem, ListItemText, useMediaQuery, ListItemIcon } from '@mui/material';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
-
+import MenuIcon from '@mui/icons-material/Menu';
 import './Navbar.scss';
 
 const Navbar = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const isMobileView = useMediaQuery('(max-width:600px)');
+
+  const toggleDrawer = (open) => (event) => {
+    setIsDrawerOpen(open);
+  };
+
   return (
-    <Grid container alignItems={'center'}  className='Navbar--container'>
+    <Grid container alignItems="center" className="Navbar--container">
       <Grid item xs={2}>
         <Logo />
       </Grid>
-      <Grid item xs={7} display='flex' justifyContent='center' className='Navbar--links'>
-        <Box p={4}>Home</Box>
-        <Box p={4}>FAQ</Box>
-        <Box p={4}>Events</Box>
-      </Grid>
-      <Grid item xs={3} display='flex' justifyContent='flex-end' className='Navbar--links'>
-      <Box p={2}><ShoppingCartOutlinedIcon/></Box>
-      <Box p={2}><LogoutOutlinedIcon/></Box>
-      </Grid>
-    </Grid>
-  )
-}
+      {!isMobileView && (
+        <>
 
-export default Navbar
+          <Grid item xs={7} display="flex" justifyContent="center" className="Navbar--links">
+            <Box p={4}>Home</Box>
+            <Box p={4}>FAQ</Box>
+            <Box p={4}>Events</Box>
+          </Grid>
+          <Grid item xs={3} display="flex" justifyContent="flex-end" className="Navbar--links">
+            <Box p={2}>
+              <ShoppingCartOutlinedIcon />
+            </Box>
+            <Box p={2}>
+              <LogoutOutlinedIcon />
+            </Box>
+          </Grid>
+        </>
+      )}
+      {isMobileView && (
+        <Grid item xs display='flex' justifyContent={'flex-end'}>
+          <IconButton onClick={toggleDrawer(true)} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+        </Grid>
+      )}
+
+      <Drawer anchor="right" open={isDrawerOpen} onClose={toggleDrawer(false)}>
+        <List>
+          {[
+            { text: 'Home' },
+            { text: 'FAQ' },
+            { text: 'Events' },
+            { icon: <ShoppingCartOutlinedIcon /> },
+            { icon: <LogoutOutlinedIcon /> }
+          ].map((item, index) => (
+            <>
+            <ListItem button key={item.text}>
+              <ListItemText primary={item.text} />
+            </ListItem>
+            {item.icon &&
+              <ListItem button key={index}>
+              <ListItemIcon>{item.icon}</ListItemIcon>
+            </ListItem>
+            }
+            </>
+          ))}
+        </List>
+      </Drawer>
+    </Grid>
+  );
+};
+
+export default Navbar;
