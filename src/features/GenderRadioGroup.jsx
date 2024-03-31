8019054fd3440c476eddc31636c0f842883f976a
vamshisai98 <saivamshianirudh136@gmail.com>
2024-03-31 13:05:02 +0530

    Gender radio group styling completed

--- src/features/GenderRadioGroup.jsx
+++ src/features/GenderRadioGroup.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { RadioGroup, FormControlLabel, Radio, Typography, Box } from '@mui/material';
+import { RadioGroup, FormControlLabel, Radio, Typography, Box, Grid } from '@mui/material';
+
+import './GenderRadioGroup.scss'
 
 const GenderRadioGroup = ({ value, onChange }) => {
   return (
@@ -12,8 +14,35 @@ const GenderRadioGroup = ({ value, onChange }) => {
         onChange={onChange}
         row
       >
-        <FormControlLabel value="male" control={<Radio />} label="Male" />
-        <FormControlLabel value="female" control={<Radio />} label="Female" />
+        <Grid container spacing={3}>
+          <Grid item xs={6} >
+            <FormControlLabel value="male" control={<Radio sx={{ color: 'white !important' }}/>} label="Male" sx={{
+              flexDirection: 'row-reverse',
+              justifyContent: 'space-between',
+              width: '100%',
+              background:'#50A7DA',
+              padding:'7px',
+              marginLeft:0,
+              borderRadius:'4px',
+              color:'white'
+            }}
+              labelPlacement="start" />
+          </Grid>
+          <Grid item xs={6}>
+
+            <FormControlLabel value="female" control={<Radio sx={{ color: 'white !important' }} />} label="Female" sx={{
+              flexDirection: 'row-reverse',
+              justifyContent: 'space-between',
+              width: '100%',
+              background: '#F8BBD0',
+              padding:'7px',
+              marginLeft:0,
+              borderRadius:'4px',
+              color:'white'
+            }}
+              labelPlacement="start" />
+          </Grid>
+        </Grid>
       </RadioGroup>
     </Box>
   );
